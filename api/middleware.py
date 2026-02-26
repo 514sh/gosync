1daa9819daa12fa4a80e4ececbe46a1d0501fddd
514sh <mark.joseph.balagtas@outlook.com>
2026-02-27 00:03:59 +0800

    fix: middleware - Tenant context in JWT or Session

--- api/middleware.py
+++ api/middleware.py
@@ -8,32 +8,40 @@ def set_tenant_context_middleware(get_response):
 
     def middleware(request):
         # Code to be executed for each request before
-        # the view (and later middleware) are called.
-        if not request.path.startswith("/api/projects/"):
-            return get_response(request)
-        user_auth_tuple = JWTAuthentication().authenticate(Request(request))
+        tenant_id = None
+        user_id = None
 
-        if user_auth_tuple is not None:
-            # If authentication is successful, set request.user and request.auth
-            request.user, request.auth = user_auth_tuple
-        print(request.user)
-        user_id = request.auth.payload.get("user_id") if request.auth else None
+        # 1. Get user_id via JWT Token
+        if "Authorization" in request.headers:
+            user_auth_tuple = JWTAuthentication().authenticate(Request(request))
+            if user_auth_tuple is not None:
+                request.user, request.auth = user_auth_tuple
+                user_id = request.auth.payload.get("user_id") if request.auth else None
+
+        # 2. If user_id is not found in JWT, check if user is authenticated via session
+        if not user_id and request.user.is_authenticated:
+            user_id = request.user.id
+
+        # 3. If user_id is found, set tenant context
         if user_id:
             from api.models import User
 
             try:
                 user = User.objects.get(id=user_id)
                 request.user = user
+                tenant_id = request.user.tenant.id
             except User.DoesNotExist:
                 request.user = None
         else:
             request.user = None
-        if request.user and request.user.tenant:
+
+        # 4. Set tenant context for the request
+        if tenant_id:
             with transaction.atomic():
                 with connection.cursor() as cursor:
                     cursor.execute(
                         "SET LOCAL app.current_tenant_id = %s",
-                        [str(request.user.tenant.id)],
+                        [str(tenant_id)],
                     )
                     print("before")
                     response = get_response(request)
